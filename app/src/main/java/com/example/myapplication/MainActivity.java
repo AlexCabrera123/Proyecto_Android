8eb3c79b0d47d59bcb98a170049abb18384744a2
AlexCabrera123 <100866047+AlexCabrera123@users.noreply.github.com>
2022-09-06 10:01:02 -0500

    Trim

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -3,33 +3,50 @@ package com.example.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.myapplication.Clases.Circulo;
 import com.example.myapplication.Clases.Triangulo;
 
 public class MainActivity extends AppCompatActivity {
 
-    private TextView tvFibo;
+    private EditText etEmail, etPassword;
+    private Button bIngresar;
+    private TextView tvPrueba;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Triangulo tri1 = new Triangulo( 12, 15,"Azul");
-        Triangulo tri2 = new Triangulo(15.5, 24, "Amarillo");
+        etEmail=findViewById(R.id.etEmail);
+        etPassword=findViewById(R.id.etPassword);
+        bIngresar=findViewById(R.id.bIngresar);
+        tvPrueba = findViewById(R.id.tvPrueba);
 
-        Circulo cir1 = new Circulo(5, 3.1416,"Azul");
-        Circulo cir2 = new Circulo(6,3.1416,"Rojo");
 
-        tri1.setColor("Negro");
+        bIngresar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
 
-        System.out.println( tri1.getColor());
 
-        tvFibo = findViewById(R.id.tvFibonacci);
-        tvFibo.setText("El àrea del circulo "+cir1.getColor()+" es"+cir1.calcularAreaCirculo());
 
+               String email = etEmail.getText().toString().trim();
+               String password = etPassword.getText().toString().trim();
+
+                if(email.equals(" ")||password.equals(" "))
+                    Toast.makeText(getApplicationContext(),"FAVOR DE INGRESAR SU EMAIL Y CONTRASEÑA",Toast.LENGTH_LONG);
+                else
+                    Toast.makeText(getApplicationContext(),email+" - "+password, Toast.LENGTH_LONG);
+
+
+
+            }
+        });
 
     }
 }
